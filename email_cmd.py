154ae30d6231f97dc5d2eba51a78ec8b188e82a1
bkfoe <tirrell.barronette@gmail.com>
2023-09-23 01:22:18 -0400

    Extracted images from the webcam video and saved them in a folder. Updated email_cmd function to send out an email with the attachment.

--- email_cmd.py
+++ email_cmd.py
@@ -1,2 +1,29 @@
-def send_email():
-    print("Email was successfully sent!")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "xguwhtthdkeflwna"
+SENDER = "bkfoepython@gmail.com"
+RECEIVER = "bkfoepython@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New object recognized in camera"
+    email_message.set_content("We recognized activity on your "
+                              "camera. It looks like an object has "
+                              "entered the frame")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/34.png")
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import cv2
 import time
 from email_cmd import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
@@ -21,7 +23,7 @@ while True:
 
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    cv2.imshow("My video", dil_frame)
+    cv2.imshow("My Video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -32,17 +34,20 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     # only interested in if the last two items are changing
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
     cv2.imshow("Video", frame)
-    # capture the image
-    # cv2.imwrite("Video", frame)
 
 # create keyboard key object and if user presses the q key it will break the video
     key = cv2.waitKey(1)
